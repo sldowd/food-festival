0718fbd27b2656d1d5e0f619e571f5f9d0ac75b4
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-03-25 16:36:24 -0400

    optimized images

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,38 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 
 // declare entry, output, and mode in webpack config
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: '/assets/js/schedule.js',
+        tickets: '/assets/js/tickets.js'
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace('../', '/assets')
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
